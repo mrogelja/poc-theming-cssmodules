255c1998f6ff91c34488256b4954834dee2016b4
mrogelja <mathieu.rogelja@gmail.com>
2023-04-12 15:50:21 +0200

    5 files modified, 1 added

--- modules/components/vite.config.js
+++ modules/components/vite.config.js
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
+import { dependencies } from './package.json';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    target: 'esnext',
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      name: 'Components',
+      formats: ['es'],
+    },
+    rollupOptions: {
+      external: [...Object.keys(dependencies)],
+    },
+  },
 });
